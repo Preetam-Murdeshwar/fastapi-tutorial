7de12e465db439101c888429038748258e468250
Preetam-Murdeshwar <preetham.murdeshwar@gmail.com>
2023-08-08 22:49:49 +0530

    Introduction

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Message": "Hello World"}
+
+@app.post("/")
+async def post():
+    return {"Message": "I am post method"}
+
+@app.put("/")
+async def put():
+    return {"Message": "I am put method"}
