9c881e2b2034f9441eb95aae36d9e13e7fcd3e26
Preetam-Murdeshwar <preetham.murdeshwar@gmail.com>
2023-08-09 00:08:06 +0530

    Path Parameters

--- main.py
+++ main.py
@@ -8,10 +8,29 @@ app = FastAPI()
 async def root():
     return {"Message": "Hello World"}
 
+
 @app.post("/")
 async def post():
     return {"Message": "I am post method"}
 
+
 @app.put("/")
 async def put():
     return {"Message": "I am put method"}
+
+
+@app.get("/users")
+async def get_all_user():
+    return {"Message": "All Users List"}
+
+
+@app.get("/users/me")
+async def get_current_user():
+    return {"Message": "I am Current User"}
+
+
+# path parameters example
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    return {"User ID": user_id}
+
