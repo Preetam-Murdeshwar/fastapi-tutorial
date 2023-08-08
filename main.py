739dd4e854feb0aa0512ba6217810f3ba7e60dcf
Preetam-Murdeshwar <preetham.murdeshwar@gmail.com>
2023-08-09 01:02:27 +0530

    Query Parameters

--- main.py
+++ main.py
@@ -34,3 +34,17 @@ async def get_current_user():
 async def get_user(user_id: str):
     return {"User ID": user_id}
 
+
+# query parameters example
+@app.get("/tiems")
+async def get_item_list(skip: int=0, limit: int=10):
+    return {"Skip": skip, "Limit": limit}
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str, q: str | None = None): # | None = None makes parameters optional
+    if q:
+        return {"Item ID:": item_id, "q": q}
+    return {"Item ID:": item_id}
+ 
+
