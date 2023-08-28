b374d2286fb0c58ff1e1cc1fe4e690827717d04f
Preetam-Murdeshwar <preetham.murdeshwar@gmail.com>
2023-08-28 14:05:32 +0530

    request body example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+# from typing import Optional
 
 
 app = FastAPI()
@@ -35,16 +37,29 @@ async def get_user(user_id: str):
     return {"User ID": user_id}
 
 
-# query parameters example
-@app.get("/tiems")
-async def get_item_list(skip: int=0, limit: int=10):
-    return {"Skip": skip, "Limit": limit}
+# # query parameters example
+# @app.get("/tiems")
+# async def get_item_list(skip: int=0, limit: int=10):
+#     return {"Skip": skip, "Limit": limit}
+ 
+
+# @app.get("/items/{item_id}")
+# async def get_item(item_id: str, q: str | None = None): # | None = None makes parameters optional works python > 3.10
+#     if q:
+#         return {"Item ID:": item_id, "q": q}
+#     return {"Item ID:": item_id}
 
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: str, q: str | None = None): # | None = None makes parameters optional
-    if q:
-        return {"Item ID:": item_id, "q": q}
-    return {"Item ID:": item_id}
+# request body
+class Item(BaseModel):
+    name: str
+    description: str | None = None  # Optional[str] = None - Used if python < 3.10. This makes the parameter optional
+    price: float
+    tax: float | None = None # Makes the parameter optional
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    return item
  
 
